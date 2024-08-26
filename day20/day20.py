5df9590d5d6fafb326607f314ea344c57a6a8230
filfilovski <pilipe2727@gmail.com>
2024-08-26 20:00:37 +0400

    second commit

---
+++ day20/day20.py
@@ -0,0 +1,9 @@
+numb = input("enteк numb : ")
+if int(numb) == 20 :
+    print("you win")
+elif int(numb) == 100 :
+    print("you win")
+elif int(numb) == 200 :
+    print("you win")
+elif int(numb) == 300 :
+    print("you win")
---
+++ day20/day20_2.py
@@ -0,0 +1,7 @@
+numb = input("enteк numb : ")
+if numb > 0:
+    print(">")
+elif numb > 0:
+    print("<")
+else:
+    print("00")
